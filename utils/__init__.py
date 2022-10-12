54bc559f6630f6bb880850960d237a50b82769a2
mc_fdc <Masato.04.11.2007@gmail.com>
2022-10-13 07:55:35 +0900

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -4,4 +4,7 @@ from rt_lib.common import database
 DatabaseManager = database.DatabaseManager
 cursor = database.cursor
 
-__all__ = ("DatabaseManager",)
+__all__ = (
+    "DatabaseManager",
+    "cursor"
+)
